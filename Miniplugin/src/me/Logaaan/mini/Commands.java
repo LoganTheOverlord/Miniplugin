692043cde278fed60b9e4fbdbae699a7f6282b97
LoganTheOverlord <42440842+LoganTheOverlord@users.noreply.github.com>
2018-11-28 10:29:37 +0100

    Add files via upload

---
+++ Miniplugin/src/me/Logaaan/mini/Commands.java
@@ -0,0 +1,36 @@
+package me.Logaaan.mini;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Commands implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender s, Command c, String label, String[] args) {
+		if (s instanceof Player) {
+			Player p = (Player) s;
+			ItemStack sw = new ItemStack(Material.DIAMOND_SWORD);
+			ItemMeta mt = sw.getItemMeta();
+			mt.setDisplayName(ChatColor.RED+"Custom Sword");
+			List<String> lore = new ArrayList<String>();
+			lore.add(ChatColor.GRAY+"Zivotnost: 10/10");
+			mt.setLore(lore);
+			sw.setItemMeta(mt);
+			p.getInventory().addItem(sw);
+		}
+		
+		
+		return true;
+		
+	}
+}
--- Miniplugin/src/me/Logaaan/mini/Main.java
+++ Miniplugin/src/me/Logaaan/mini/Main.java
@@ -1,36 +1,35 @@
 package me.Logaaan.mini;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.Chunk;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import io.lumine.xikage.mythicmobs.MythicMobs;
+import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobDeathEvent;
 import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
 import io.lumine.xikage.mythicmobs.mobs.MobManager;
-import io.lumine.xikage.mythicmobs.mobs.MythicMob;
 import net.md_5.bungee.api.ChatColor;
 
-public class Main extends JavaPlugin implements Listener, CommandExecutor{
+public class Main extends JavaPlugin implements Listener{
 	
-	List<ActiveMob> mobs = new ArrayList<ActiveMob>();
+	Map<Chunk,ActiveMob> mobs = new HashMap<Chunk,ActiveMob>();
 	
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
-		this.getCommand("givemec").setExecutor(this);
+		this.getCommand("givemec").setExecutor(new Commands());
 	}
 	
 	public void onDisable() {
@@ -39,13 +38,14 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor{
 	
 	@EventHandler
 	public void shiftRight(PlayerInteractEvent e) {
-		if (e.getPlayer().isSneaking()) {
+		Player p = e.getPlayer();
+		if (p.isSneaking()) {
 			if (e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-				if (e.getPlayer().getItemInHand().hasItemMeta() && e.getPlayer().getItemInHand().getItemMeta().hasLore()) {
-					if (e.getPlayer().getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.RED+"Custom Sword")) {
+				if (p.getItemInHand() != null && p.getItemInHand().hasItemMeta() && p.getItemInHand().getItemMeta().hasLore()) {
+					if (p.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.RED+"Custom Sword")) {
 						MobManager mm = MythicMobs.inst().getMobManager();
 						ActiveMob mob = mm.spawnMob("testmob", e.getPlayer().getLocation());
-						mobs.add(mob);
+						mobs.put(e.getPlayer().getLocation().getChunk(), mob);
 					}
 				}
 			}
@@ -53,68 +53,61 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor{
 	}
 	
 	@EventHandler
-	public void onmDeath(EntityDeathEvent e) {
-		if (mobs.contains(MythicMobs.inst().getMobManager().getActiveMob(e.getEntity().getUniqueId()).get())) {
-			e.getEntity().getKiller().sendMessage("GG");
+	public void onChunkUnload(ChunkUnloadEvent e) {
+		if (mobs.containsKey(e.getChunk())) {
+			for (Chunk c : mobs.keySet()) {
+				ActiveMob m = mobs.get(c);
+				m.setDespawned();
+				mobs.remove(c);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onmDeath(MythicMobDeathEvent e) {
+		if (mobs.containsValue(e.getMob())) {
+			e.getKiller().sendMessage("GG");
+			mobs.values().remove(e.getMob());
 		}
 	}
 	
 	@EventHandler
 	public void hit(EntityDamageByEntityEvent e) {
 		if (e.getDamager() instanceof Player) {
-			ItemStack zbran = ((Player) e.getDamager()).getInventory().getItemInMainHand();
-			if (zbran.hasItemMeta() && zbran.getItemMeta().hasLore()) {
-				List<String> lore = zbran.getItemMeta().getLore();
-				int index = 0;
-				int r_l = 0;
-				int rem = 99999; //max dur.
-				int w = 0; //zbyva
-				boolean dobreak = false;
-				for (String l : lore) {
-					if (l.contains("Zivotnost")) {
-						r_l = index;
-						l = ChatColor.stripColor(l);
-						String[] well = l.split(" ");
-						String[] ziv = well[1].split("/");
-						w = Integer.parseInt(ziv[0]);
-						rem = Integer.parseInt(ziv[1]);
-						w -= 1;
-						if (w == 0) {
-							dobreak = true;
+			Player p = (Player) e.getDamager();
+			if (p.getItemInHand() != null)  {
+				ItemStack zbran = p.getInventory().getItemInMainHand();
+				if (zbran.hasItemMeta() && zbran.getItemMeta().hasLore()) {
+					List<String> lore = zbran.getItemMeta().getLore();
+					int index = 0;
+					int index_zivotnost = 0;
+					int max_dur = 99999; //max dur.
+					int remaining = 0; //zbyva
+					boolean dobreak = false;
+					for (String l : lore) { //je to tak lepší, protože mám rovnou string a nemusím ho získávat
+						if (l.contains("Zivotnost")) {
+							index_zivotnost = index;
+							l = ChatColor.stripColor(l);
+							String[] well = l.split(" ");
+							String[] ziv = well[1].split("/");
+							remaining = Integer.parseInt(ziv[0]);
+							max_dur = Integer.parseInt(ziv[1]);
+							remaining -= 1;
+							if (remaining == 0) {
+								p.getInventory().remove(p.getItemInHand());
+							} else  {
+								ItemMeta m = zbran.getItemMeta(); 
+								lore.set(index_zivotnost, ChatColor.GRAY+"Zivotnost: "+remaining+"/"+max_dur);
+								m.setLore(lore); //protože nechceme, aby se lore breaknul (tøeba staty) -> replacneme urèitou øádku.
+								p.getItemInHand().setItemMeta(m);
+							}
 						}
+						index++;
 					}
-					index++;
-				}
-				if (w > 0) {
-					lore.set(r_l, ChatColor.GRAY+"Zivotnost: "+w+"/"+rem);
-					ItemMeta m = zbran.getItemMeta();
-					m.setLore(lore);
-					zbran.setItemMeta(m);
-					((Player) e.getDamager()).getItemInHand().setItemMeta(m);
-				}
-				if (dobreak == true) {
-					((Player) e.getDamager()).getInventory().remove(((Player) e.getDamager()).getItemInHand());
 				}
 			}
 		}
 	}
 
-	@Override
-	public boolean onCommand(CommandSender s, Command c, String label, String[] args) {
-		if (s instanceof Player) {
-			Player p = (Player) s;
-			ItemStack sw = new ItemStack(Material.DIAMOND_SWORD);
-			ItemMeta mt = sw.getItemMeta();
-			mt.setDisplayName(ChatColor.RED+"Custom Sword");
-			List<String> lore = new ArrayList<String>();
-			lore.add(ChatColor.GRAY+"Zivotnost: 10/10");
-			mt.setLore(lore);
-			sw.setItemMeta(mt);
-			p.getInventory().addItem(sw);
-		}
-		
-		
-		return true;
-		
-	}
+
 }
